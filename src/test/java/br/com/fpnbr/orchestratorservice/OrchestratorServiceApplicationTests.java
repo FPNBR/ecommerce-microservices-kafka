8f93fb6f666bb776a2b3a5ff67b0f34346cb82e2
FPNBR <felipepny@gmail.com>
2024-01-17 10:33:10 -0300

    Initial commit

---
+++ src/test/java/br/com/fpnbr/orchestratorservice/OrchestratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fpnbr.orchestratorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

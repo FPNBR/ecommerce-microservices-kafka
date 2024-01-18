39a5e32d787041ee5527079faf4fb5ef6e6d9972
FPNBR <felipepny@gmail.com>
2024-01-18 17:08:24 -0300

    feat: Producer Kafka.

---
+++ src/main/java/br/com/fpnbr/orchestratorservice/kafka/producer/SagaOrchestratorProducer.java
@@ -0,0 +1,23 @@
+package br.com.fpnbr.orchestratorservice.kafka.producer;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@AllArgsConstructor
+@Component
+public class SagaOrchestratorProducer {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendEvent(String payload, String topic) {
+        try {
+            log.info("Sending event to topic: {}, with data {}", topic, payload);
+            kafkaTemplate.send(topic, payload);
+        } catch (Exception e) {
+            log.error("Error sending event to topic: {}, with data {}", topic, payload, e);
+        }
+    }
+}

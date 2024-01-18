471f92ea1fa8fa2f3c2cd8c82b9c361f72f25e84
FPNBR <felipepny@gmail.com>
2024-01-18 18:57:18 -0300

    feat: Consumer Kafka. fix: KafkaTemplate in KafkaProducer.

---
+++ src/main/java/br/com/fpnbr/paymentservice/kafka/consumer/PaymentConsumer.java
@@ -0,0 +1,29 @@
+package br.com.fpnbr.paymentservice.kafka.consumer;
+
+import br.com.fpnbr.paymentservice.util.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@AllArgsConstructor
+@Component
+public class PaymentConsumer {
+
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(groupId = "${spring.kafka.consumer.group-id", topics = "${spring.kafka.topic.payment-success}")
+    public void consumePaymentSuccessEvent(String payload) {
+        log.info("Consuming success event: {} from payment-success topic", payload);
+        var event = jsonUtil.toEventDTO(payload);
+        log.info("Event: {}", event);
+    }
+
+    @KafkaListener(groupId = "${spring.kafka.consumer.group-id", topics = "${spring.kafka.topic.payment-failure}")
+    public void consumePaymentFailureEvent(String payload) {
+        log.info("Consuming rollback event: {} from payment-failure topic", payload);
+        var event = jsonUtil.toEventDTO(payload);
+        log.info("Event: {}", event);
+    }
+}

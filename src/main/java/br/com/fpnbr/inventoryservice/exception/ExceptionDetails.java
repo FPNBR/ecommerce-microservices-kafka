8b029598c507d666a87a16af193a06d4b47d82b9
FPNBR <felipepny@gmail.com>
2024-01-18 14:08:56 -0300

    feat: Json Util and Exception Global Handler.

---
+++ src/main/java/br/com/fpnbr/inventoryservice/exception/ExceptionDetails.java
@@ -0,0 +1,4 @@
+package br.com.fpnbr.inventoryservice.exception;
+
+public record ExceptionDetails(int status, String message) {
+}
---
+++ src/main/java/br/com/fpnbr/inventoryservice/util/JsonUtil.java
@@ -0,0 +1,33 @@
+package br.com.fpnbr.inventoryservice.util;
+
+import br.com.fpnbr.inventoryservice.dto.EventDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@AllArgsConstructor
+@Component
+public class JsonUtil {
+
+    private final ObjectMapper objectMapper;
+
+    public String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception e) {
+            log.error("Erro ao converter para JSON: {}", e.getMessage());
+            return "";
+        }
+    }
+
+    public EventDTO toEventDTO(String json) {
+        try {
+            return objectMapper.readValue(json, EventDTO.class);
+        } catch (Exception e) {
+            log.error("Erro ao converter para Objeto: {}", e.getMessage());
+            return null;
+        }
+    }
+}

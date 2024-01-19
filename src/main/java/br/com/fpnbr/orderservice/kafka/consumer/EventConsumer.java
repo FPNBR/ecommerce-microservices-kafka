7e63c5535e10d7df3b372cc9dd64a2b43612e823
FPNBR <felipepny@gmail.com>
2024-01-19 15:36:36 -0300

    feat: Logic for receiving notification of Saga completion.

--- src/main/java/br/com/fpnbr/orderservice/kafka/consumer/EventConsumer.java
+++ src/main/java/br/com/fpnbr/orderservice/kafka/consumer/EventConsumer.java
@@ -1,5 +1,6 @@
 package br.com.fpnbr.orderservice.kafka.consumer;
 
+import br.com.fpnbr.orderservice.service.EventService;
 import br.com.fpnbr.orderservice.util.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,11 +13,12 @@ import org.springframework.stereotype.Component;
 public class EventConsumer {
 
     private final JsonUtil jsonUtil;
+    private final EventService eventService;
 
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id", topics = "${spring.kafka.topic.notify-ending}")
     public void consumeNotifyEndingEvent(String payload) {
         log.info("Consuming event: {} from notify-ending topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info("Event: {}", event);
+        eventService.notifyEnding(event);
     }
 }
--- src/main/java/br/com/fpnbr/orderservice/service/EventService.java
+++ src/main/java/br/com/fpnbr/orderservice/service/EventService.java
@@ -3,8 +3,12 @@ package br.com.fpnbr.orderservice.service;
 import br.com.fpnbr.orderservice.domain.document.Event;
 import br.com.fpnbr.orderservice.repository.EventRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @AllArgsConstructor
 @Service
 public class EventService {
@@ -13,5 +17,12 @@ public class EventService {
 
     public Event saveEvent(Event event) {
         return eventRepository.save(event);
+    }
+
+    public void notifyEnding(Event event) {
+        event.setOrderId(event.getOrderId());
+        event.setCreatedAt(LocalDateTime.now());
+        eventRepository.save(event);
+        log.info("Order {} with saga notified! TransactionId: {}", event.getOrderId(), event.getTransactionId());
     }
 }

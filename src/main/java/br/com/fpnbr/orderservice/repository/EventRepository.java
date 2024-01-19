2f05044f84464605638abf9392cc407958fbd62f
FPNBR <felipepny@gmail.com>
2024-01-19 17:07:01 -0300

    feat: REST endpoints for making a request (POST) and fetching events (GET).

--- src/main/java/br/com/fpnbr/orderservice/repository/EventRepository.java
+++ src/main/java/br/com/fpnbr/orderservice/repository/EventRepository.java
@@ -4,6 +4,14 @@ import br.com.fpnbr.orderservice.domain.document.Event;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface EventRepository extends MongoRepository<Event, String> {
+    List<Event> findAllByOrderByCreatedAtDesc();
+
+    Optional<Event> findFirstByOrderIdOrderByCreatedAtDesc(String orderId);
+
+    Optional<Event> findFirstByTransactionIdOrderByCreatedAtDesc(String transactionId);
 }

4f4c0acf5562d1dd327ca8d09ec1a7fae86afb94
FPNBR <felipepny@gmail.com>
2024-01-22 22:13:23 -0300

    refactor: Changed table name in import.sql.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.product (id, code) VALUES (1, 'COMIC_BOOKS');
-INSERT INTO public.product (id, code) VALUES (2, 'BOOKS');
-INSERT INTO public.product (id, code) VALUES (3, 'MOVIES');
-INSERT INTO public.product (id, code) VALUES (4, 'MUSIC');
+INSERT INTO public.t_product (id, code) VALUES (1, 'COMIC_BOOKS');
+INSERT INTO public.t_product (id, code) VALUES (2, 'BOOKS');
+INSERT INTO public.t_product (id, code) VALUES (3, 'MOVIES');
+INSERT INTO public.t_product (id, code) VALUES (4, 'MUSIC');

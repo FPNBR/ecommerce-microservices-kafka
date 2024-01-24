a1c09ec05d82ff7f9a6231690655fa802c688c38
FPNBR <felipepny@gmail.com>
2024-01-24 20:25:27 -0300

    feat: OrchestratorService and SagaExecutionController to orchestrate all services.

--- src/main/java/br/com/fpnbr/orchestratorservice/dto/EventDTO.java
+++ src/main/java/br/com/fpnbr/orchestratorservice/dto/EventDTO.java
@@ -8,7 +8,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Data
 @Builder
@@ -24,4 +27,11 @@ public class EventDTO {
     private SagaStatusEnum status;
     private List<HistoryDTO> eventHistory;
     private LocalDateTime createdAt;
+
+    public void addToHistory(HistoryDTO history) {
+        if (isEmpty(eventHistory)) {
+            eventHistory = new ArrayList<>();
+        }
+        eventHistory.add(history);
+    }
 }
--- src/main/java/br/com/fpnbr/orchestratorservice/kafka/consumer/SagaOrchestratorConsumer.java
+++ src/main/java/br/com/fpnbr/orchestratorservice/kafka/consumer/SagaOrchestratorConsumer.java
@@ -1,5 +1,6 @@
 package br.com.fpnbr.orchestratorservice.kafka.consumer;
 
+import br.com.fpnbr.orchestratorservice.service.OrchestratorService;
 import br.com.fpnbr.orchestratorservice.util.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,33 +12,34 @@ import org.springframework.stereotype.Component;
 @Component
 public class SagaOrchestratorConsumer {
 
+    private final OrchestratorService orchestratorService;
     private final JsonUtil jsonUtil;
 
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id", topics = "${spring.kafka.topic.start-saga}")
     public void consumeStartSagaEvent(String payload) {
         log.info("Consuming event: {} from start-saga topic", payload);
         var event = jsonUtil.toEventDTO(payload);
-        log.info("Event: {}", event);
+        orchestratorService.startSataga(event);
     }
 
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id", topics = "${spring.kafka.topic.orchestrator}")
     public void consumeOrchestratorSagaEvent(String payload) {
         log.info("Consuming event: {} from orchestrator topic", payload);
         var event = jsonUtil.toEventDTO(payload);
-        log.info("Event: {}", event);
+        orchestratorService.continueSaga(event);
     }
 
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id", topics = "${spring.kafka.topic.finish-success}")
     public void consumeFinishSuccessEvent(String payload) {
         log.info("Consuming event: {} from finish-success topic", payload);
         var event = jsonUtil.toEventDTO(payload);
-        log.info("Event: {}", event);
+        orchestratorService.finishSagaSuccess(event);
     }
 
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id", topics = "${spring.kafka.topic.finish-failure}")
     public void consumeFinishFailureEvent(String payload) {
         log.info("Consuming event: {} from finish-failure topic", payload);
         var event = jsonUtil.toEventDTO(payload);
-        log.info("Event: {}", event);
+        orchestratorService.finishSagaFailure(event);
     }
 }
